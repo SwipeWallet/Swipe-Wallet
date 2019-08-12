e8542c8b2ae37c8d4482c8071d0f1db14051059d
SwipeDevelopment <52673790+SwipeDevelopment@users.noreply.github.com>
2019-08-12 22:03:09 +1000

    add deposit and activate fee functions

--- RequestAllowance.sol
+++ RequestAllowance.sol
@@ -3,29 +3,74 @@ pragma solidity ^0.5.0;
 import "./SwipeToken.sol";
 
 contract RequestAllowance is Owned {
+    using SafeMath for uint;
+       
     SwipeToken token;
-    uint fee = 20;
+    address feeAdmin;
     mapping(address => bool) allowedUsers;
+    mapping(address => uint) feeBalances;
     event Approval(address indexed tokenOwner);
 
-    // Create a new RequestAllowance
-    constructor(address payable addrToken, uint _fee) public {
+    /// Create a new RequestAllowance
+    constructor(address payable addrToken, address _feeAdmin) public {
         token = SwipeToken(addrToken);
-        fee = _fee;
+        feeAdmin = _feeAdmin;
+    }
+    
+    function setFeeAdmin(address _feeAdmin) public onlyOwner {
+        feeAdmin = _feeAdmin;
     }
 
     function allowedOf(address user) public view returns(bool) {
         return allowedUsers[user];    
     }
-    
-    function requestAllowance() public {
+
+    function feeBalanceOf(address user) public view returns(uint) {
+        return feeBalances[user];    
+    }
+
+    // To get success, user need to approve 1 token to smart contract
+    function requestAllowance() public returns (bool success) {
         require(allowedUsers[msg.sender] == false, 'user is already allowed');
-        require(token.balanceOf(msg.sender) >= fee, 'user balance is not sufficient');
+        require(token.balanceOf(msg.sender) >= 1, 'user balance is not sufficient');
+        require(token.allowance(msg.sender, address(this)) >= 1, 'token is not allowd to smart contract');
         
-        if (token.burnForAllowance(msg.sender, owner, fee)) {
+        if (token.transferFrom(msg.sender, address(this), 1)) {
             allowedUsers[msg.sender] = true;
             emit Approval(msg.sender);
+            return true;
         }
+        
+        return false;
+    }
+
+    // To get success, user need to approve tokens to smart contract
+    function depositForFee(uint tokenAmount) public returns (bool success) {
+        require(allowedUsers[msg.sender] == true, 'user is not allowed');
+        require(token.balanceOf(msg.sender) >= tokenAmount, 'user balance is not sufficient');
+        require(token.allowance(msg.sender, address(this)) >= tokenAmount, 'tokens are not allowd to smart contract');
+        
+        if (token.transferFrom(msg.sender, address(this), tokenAmount)) {
+            feeBalances[msg.sender] = feeBalances[msg.sender].add(tokenAmount);
+            return true;
+        }
+        
+        return false;
+    }
+    
+    function payFee(uint tokenAmount) public returns (bool success) {
+        require(allowedUsers[msg.sender] == true, 'user is not allowed');
+        require(feeBalances[msg.sender] >= tokenAmount, 'not enough fee balance');
+        
+        uint burnAmount = tokenAmount.mul(8).div(10);
+        uint fee = tokenAmount - burnAmount;
+        if (token.burn(burnAmount)) {
+            token.transfer(feeAdmin, fee);
+            feeBalances[msg.sender] = feeBalances[msg.sender].sub(tokenAmount);
+            return true;
+        }
+        
+        return false;
     }
     
     // ------------------------------------------------------------------------
